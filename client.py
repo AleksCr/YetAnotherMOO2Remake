11eba5dfde0a5ae210b963101503ef87a8172bcb
alisitsky <alisitsky@webim.ru>
2018-12-09 23:13:10 +0300

    YAMOO2R-2 совсем совсем тестовый код

--- client.py
+++ client.py
@@ -13,24 +13,43 @@ def client_main_cycle():
 
     running = True
 
+    server = socket.socket()
+    server.connect(('localhost', 9901))
+
     while running:
+        pygame.time.Clock().tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                server.close()
                 running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                # print(pos)
+                visual_data = send_json_data(server, {'newship': pos})
+                if not visual_data:
+                    continue
+                visual_data = visual_data['full_update']
+                for data in visual_data:
+                    for type, coords in data.items():
+                        heh = type
+                        peh = coords
+                        screen.blit(logo, peh)
+
+        pygame.display.flip()
 
 
-def send_json_data():
-    server = socket.socket()
-    server.connect(('localhost', 1011))
-
-    send_data = {'data': 'something'}
+def send_json_data(server, send_data):
     s = json.dumps(send_data)
-    server.sendto(s.encode(), ('localhost', 1011))
+    server.sendto(s.encode(), ('localhost', 9901))
 
-    response_data = server.recv(1024)
-    server.close()
+    response_data = server.recv(2048)
 
+    if not response_data:
+        return
+
+    response_data = json.loads(response_data)
     print(response_data)
+    return response_data
 
 
 def client():
@@ -39,4 +58,3 @@ def client():
 
 if __name__ == "__main__":
     client_main_cycle()
-    send_json_data()
--- server.py
+++ server.py
@@ -1,28 +1,59 @@
 import socket
 import json
+import time
+
+ships = []
+
+
+class Ship:
+
+    def __init__(self, pos):
+        self.x, self.y = pos
+
+    def get_pos(self):
+        return self.x, self.y
 
 
 def server():
     sock = socket.socket()
-    sock.bind(('', 1011))
+    sock.bind(('', 9901))
     sock.listen(1)
-    conn, addr = sock.accept()
-
-    print('connected:', addr)
 
     while True:
-        raw_data = conn.recv(2048)
-        print(raw_data)
 
-        if not raw_data:
-            break
+        print('test')
+        conn, addr = sock.accept()
+        print('ree')
 
-        resp_json = {'message': 'ok'}
-        response_data = json.dumps(resp_json)
-        conn.sendto(response_data.encode(), addr)
-        print(response_data)
+        while True:
+            try:
+                raw_data = conn.recv(2048)
+            except socket.error:
+                print('client disconnected')
+                conn.close()
+                break
 
-    conn.close()
+            if not raw_data:
+                break
+
+            print(raw_data)
+            raw_data = json.loads(raw_data)
+            if raw_data['newship']:
+                print(raw_data['newship'])
+                sh = Ship(raw_data['newship'])
+                ships.append(sh)
+
+            full_load_json = []
+            for ship in ships:
+                full_load_json.append({'ship': ship.get_pos()})
+            full_load_json = {'full_update': full_load_json}
+            print(full_load_json)
+            resp_json = {'message': 'ok'}
+            response_data = json.dumps(resp_json)
+            full_loadd_json = json.dumps(full_load_json)
+            conn.sendto(full_loadd_json.encode(), addr)
+            print(response_data)
+    print('kek')
 
 
 if __name__ == "__main__":
